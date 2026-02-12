db5787a26e642f2c2c32b6c3d9fa954c6733d0e6
Billy E. Whited <billy@deadapp.info>
2026-02-12 04:50:57 -0600

    changed rootlayout

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -17,6 +17,7 @@ import {
 } from "lucide-react";
 import { Card } from "./ui/card";
 import { MobileInstallModal } from './MobileInstallModal';
+import { MobileInstall } from './ModbileInstallModal';
 import { Badge } from "./ui/badge";
 import { Progress } from "./ui/progress";
 import { useState } from "react";
